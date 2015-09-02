06f960db4ae435cf4f078c6e1513475c29916733
CkR-0ff <gregoslp@gmail.com>
2015-09-02 14:51:17 +0300

    BrandNewPage

--- javascripts/script.js
+++ javascripts/script.js
@@ -50,3 +50,18 @@ $(document).ready(function(){
   })
 });
 })(jQuery)
+
+function loadXMLDoc(dname) 
+{
+if (window.XMLHttpRequest)
+  {
+  xhttp=new XMLHttpRequest();
+  }
+else
+  {
+  xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xhttp.open("GET",dname,false);
+xhttp.send();
+return xhttp.responseXML;
+}
