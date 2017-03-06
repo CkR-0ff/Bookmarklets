a55cde0c45dafef49a5ac7490c74c35e1f10de31
CkR-0ff <CkR-0ff@users.noreply.github.com>
2017-03-06 09:58:23 +0200

    Update ElemRemover.js

--- jsFiles/ElemRemover.js
+++ jsFiles/ElemRemover.js
@@ -1,50 +1,57 @@
-var ElementRemover = function(){/*javascript:	
-	if(!dieFunc){
-		var dieFunc = function(){
-			var curElem = undefined;
-			var stl = document.createElement('style');
-			var txtnode = document.createTextNode('body:hover .coupontooltip {display: block;} .coupontooltip {display: none; background: #C8C8C8; margin-left: 5px; padding: 0px; position: absolute; z-index: 1000;} .onhvrbrd {border-style: solid; border-width: 1px; border-color:red}');
-			stl.appendChild(txtnode);
-			document.getElementsByTagName('head')[0].appendChild(stl);
-			tooltiper = document.createElement('div');
-			tooltiper.className = 'coupontooltip';
-			tooltiper.Id = 'coupontooltip';
-			document.getElementsByTagName('body')[0].appendChild(tooltiper);
-			document.onmousemove = function(e) {  tooltiper.style.left = e.pageX + 'px';
-				tooltiper.style.top = e.pageY + 'px';
-				tooltiper.innerHTML = 'Tag Type: ' + e.path[0].tagName + '';
-				for(var i = 0; i<e.path[0].attributes.length; i++){
-					tooltiper.innerHTML += '<br>' + e.path[0].attributes[i].name + ': ' + e.path[0].attributes[i].value;
-				}        
-			};
-			
-			document.onmouseover = function(e){  
-				e.path[0].classList.add('onhvrbrd');   
-				curElem = e.path[0];
-			};      
-			document.onmouseout = function(e){ 
-				e.path[0].classList.remove('onhvrbrd');      
-			}; 
-			document.onkeyup = function (e) {
-				e = e || window.event;
-				if(e.keyCode == 46){
-					curElem.style.display = 'none';
-				}
-			};
-		};
-		dieFunc();
-	}else{
-		dieFunc = undefined;
-		document.onmousemove = function(e) {return true;      };
-		document.onmouseover = function(e) {return true;      };
-		document.onmouseout = function(e) {return true;      }; 
-		document.onkeyup = function(e) {return true;      };
-		var thetools = document.getElementsByClassName('coupontooltip'); 
-		for(var i = 0; i < thetools.length; i++){ 
-			thetools[i].parentElement.removeChild(thetools[i]);
-		}    
-	}
-	document.body.focus();
+var ElementRemover = function(){/*javascript: if (!dieFunc) {
+    var dieFunc = function() {
+        var curElem = undefined;
+        var stl = document.createElement('style');
+        var txtnode = document.createTextNode('body:hover .coupontooltip {display: block;} .coupontooltip {display: none; background: #C8C8C8; margin-left: 5px; padding: 0px; position: absolute; z-index: 1000;} .onhvrbrd {border-style: solid; border-width: 1px; border-color:red}');
+        stl.appendChild(txtnode);
+        document.getElementsByTagName('head')[0].appendChild(stl);
+        tooltiper = document.createElement('div');
+        tooltiper.className = 'coupontooltip';
+        tooltiper.Id = 'coupontooltip';
+        document.getElementsByTagName('body')[0].appendChild(tooltiper);
+        document.onmousemove = function(e) {
+            tooltiper.style.left = e.pageX + 'px';
+            tooltiper.style.top = e.pageY + 'px';
+            tooltiper.innerHTML = 'Tag Type: ' + e.path[0].tagName + '';
+            for (var i = 0; i < e.path[0].attributes.length; i++) {
+                tooltiper.innerHTML += '<br>' + e.path[0].attributes[i].name + ': ' + e.path[0].attributes[i].value;
+            }
+        };
+        document.onmouseover = function(e) {
+            e.path[0].classList.add('onhvrbrd');
+            curElem = e.path[0];
+        };
+        document.onmouseout = function(e) {
+            e.path[0].classList.remove('onhvrbrd');
+        };
+        document.onkeyup = function(e) {
+            e = e || window.event;
+            if (e.keyCode == 46) {
+                curElem.style.display = 'none';
+            }
+        };
+    };
+    dieFunc();
+} else {
+    dieFunc = undefined;
+    document.onmousemove = function(e) {
+        return true;
+    };
+    document.onmouseover = function(e) {
+        return true;
+    };
+    document.onmouseout = function(e) {
+        return true;
+    };
+    document.onkeyup = function(e) {
+        return true;
+    };
+    var thetools = document.getElementsByClassName('coupontooltip');
+    for (var i = 0; i < thetools.length; i++) {
+        thetools[i].parentElement.removeChild(thetools[i]);
+    }
+}
+document.body.focus();
 */}.toString().slice(14, -4);
 
 var WebmAutoDownload = function(){/*javascript: (
@@ -114,18 +121,38 @@ var CSSHider = function(){/*javascript: (function() {
     }
 }());*/}.toString().slice(14, -4);
 
+var YTAutoReplay = function(){/*javascript: console.log(myIntrID);
+var myIntrID = myIntrID;
+var repRun = function() {
+    if (!myIntrID) {
+        myIntrID = setInterval(function() {
+            var btns = document.getElementsByClassName('ytp-play-button ytp-button');
+            console.log(btns.length);
+            for (var i = 0; i < btns.length; i++) {
+                if (btns[i].title == "Replay" || btns[i].title == "Пускане пак") {
+                    btns[i].click();
+                }
+            }
+        }, 2000);
+    } else {
+        clearInterval(myIntrID);
+        myIntrID = undefined;
+    }
+};
+repRun();*/}.toString().slice(14, -4);
+
 function RetJson(){
   var obj = {
     "marklets": [
       {
 	"Title": "Element Remover", 
 	"Code": ElementRemover, 
-	"Description": "When activated the hovered element is outlined in red and his name is shown. On rignt click the element is removed. The functionality is removed on second click of the bookmarklet."
+	"Description": "When activated the hovered element is outlined in red and his name is shown. On Delete button press the element is removed. The functionality is removed on second click of the bookmarklet."
       },
       {
-	"Title": "4chan Auto Download", 
-	"Code": WebmAutoDownload, 
-	"Description": "Automaticaly downloads all post pictures(images, WEBMs, GIFs, etc.) in a 4chan thread. Creates and downloads 'NamesLinks.txt containig the coresponding String names to every code name of every picture.'"
+	"Title": "CSS Hider", 
+	"Code": CSSHider, 
+	"Description": "Hide elements on a page by CSS selector. The selectors are given as params to the anon function."
       },
       {
 	"Title": "Download-ready 4chan", 
@@ -133,10 +160,16 @@ function RetJson(){
 	"Description": "In 4chan thread every picture when clicked opens a 'Save As' dialog with the its page name."
       },
       {
-	"Title": "CSS Hider", 
-	"Code": CSSHider, 
-	"Description": "Hide elements on a page by CSS selector. The selectors are given as params to the anon function."
+	"Title": "4chan Auto Download", 
+	"Code": WebmAutoDownload, 
+	"Description": "Automaticaly downloads all post pictures(images, WEBMs, GIFs, etc.) in a 4chan thread. Creates and downloads 'NamesLinks.txt containig the coresponding String names to every code name of every picture.'"
+      },
+      {
+	"Title": "YouTube Auto Repaly", 
+	"Code": YTAutoReplay, 
+	"Description": "When activated on YouTube video page the video is automatically replayed when it finishes(works only on english and bulgarian languages)."
       }
+      
     ]
     
   };
